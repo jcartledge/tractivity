a0f57db0b6868078b6150b375f2d0b1720e497e2
James Cartledge <jcartledge@gmail.com>
2013-06-05 21:29:10 +1000

    wrinkle free

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,8 +1,12 @@
 $(function() {
 
-  var times = $('[datetime]');
+  var getTimes = function($el) {
+    if (!$el) $el = $('body');
+    return $el.find('[datetime]');
+  };
 
-  var updateTimes = function() {
+  var updateTimes = function(times) {
+    if(!times) times = getTimes();
     times.each(function() {
       var $this = $(this);
       $this.html(moment(fixTime($this)).fromNow());
@@ -14,16 +18,20 @@ $(function() {
   };
 
   var update = function() {
-    var since = fixTime(times.first());
+    var since = fixTime(getTimes().first());
     var url = location.href + '?' + $.param({'occurred_since_date': since});
     var insert = function(lis) {
       $(lis.pop()).hide().prependTo('.items').slideDown(function() {
-        if (lis.length) insert(lis);
+        if (lis.length) {
+          insert(lis);
+        }
       });
     };
     $.get(url, function(data) {
-      insert($(data).find('li').get());
-      updateTimes();
+      var $data = $(data);
+      updateTimes(getTimes($data));
+      insert($data.find('li').get());
+      updateTimes(getTimes());
     });
   };
 
