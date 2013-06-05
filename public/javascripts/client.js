496260ee5b28c98d6371e49974288a4ce9d0eb87
James Cartledge <jcartledge@gmail.com>
2013-06-05 20:34:20 +1000

    ajax to get new events

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,17 @@
 $(function() {
 
-});
+  var update = function() {
+    var since = $('[data-time]').first().data('time').replace(/EST$/, '+1000');
+    var url = location.href + '?' + $.param({'occurred_since_date': since});
+    var insert = function(lis) {
+      $(lis.pop()).hide().prependTo('.items').slideDown(function() {
+        if (lis.length) insert(lis);
+      });
+    };
+    $.get(url, function(data) {
+      insert($(data).find('li').get());
+    });
+  };
 
-function checkForUpdates() {
-  var since = $('[data-time]').first().data('time').replace(/ EST$/, ' +1000');
-  var url = location.href + '?' + $.param({'occurred_since_date': since});
-  $.get(url, function(data) {
-    console.log(data);
-  });
-}
+  setInterval(update, 60 * 1000);
+});
