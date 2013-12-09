ce0c77d23fca4c201df56b621f7ad3126daff791
James Cartledge <jcartledge@gmail.com>
2013-12-09 17:17:26 +1100

    style

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,49 +1,57 @@
-$(function() {
+(function($, moment) {
 
     'use strict';
 
-    function getTimes($el) {
-        if (!$el) $el = $('body');
-        return $el.find('[datetime]');
-    }
+    $(function() {
 
-    function updateTimes(times) {
-        if(!times) times = getTimes();
-        times.each(function() {
-            var $this = $(this);
-            $this.html(moment(fixTime($this)).fromNow());
-        });
-    }
+        function getTimes($el) {
+            if (!$el) {
+                $el = $('body');
+            }
+            return $el.find('[datetime]');
+        }
 
-    function fixTime($el) {
-        console.log(getTimeZone());
-        return $el.attr('datetime').replace(/EST$/, getTimeZone());
-    }
-
-    function getTimeZone() {
-        var tz = (new Date()).getTimezoneOffset() / -.6;
-        return tz < 0 ? tz : '+' + tz;
-    }
-
-    function update() {
-        var since = fixTime(getTimes().first());
-        var url = location.href + '?' + $.param({'occurred_since_date': since});
-        function insert(lis) {
-            $(lis.pop()).hide().prependTo('.items').slideDown(function() {
-                if (lis.length) {
-                    insert(lis);
-                }
+        function updateTimes(times) {
+            if(!times){
+                times = getTimes();
+            }
+            times.each(function() {
+                var $this = $(this);
+                $this.html(moment(fixTime($this)).fromNow());
             });
         }
-        $.get(url, function(data) {
-            var $data = $(data);
-            updateTimes(getTimes($data));
-            insert($data.find('li').get());
-            updateTimes(getTimes());
-        });
-    }
 
-    setInterval(update, 60 * 1000);
-    updateTimes();
+        function fixTime($el) {
+            console.log(getTimeZone());
+            return $el.attr('datetime').replace(/EST$/, getTimeZone());
+        }
 
-});
+        function getTimeZone() {
+            var tz = (new Date()).getTimezoneOffset() / -0.6;
+            return tz < 0 ? tz : '+' + tz;
+        }
+
+        function update() {
+            var since = fixTime(getTimes().first());
+            var url = location.href + '?' + $.param({'occurred_since_date': since});
+            function insert(lis) {
+                $(lis.pop()).hide().prependTo('.items').slideDown(function() {
+                    if (lis.length) {
+                        insert(lis);
+                    }
+                });
+            }
+            $.get(url, function(data) {
+                var $data = $(data);
+                updateTimes(getTimes($data));
+                insert($data.find('li').get());
+                updateTimes(getTimes());
+            });
+        }
+
+        setInterval(update, 60 * 1000);
+        updateTimes();
+
+    });
+
+}(window.jQuery, window.moment));
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,20 @@
+(function() {
 
-/*
- * GET home page.
- */
+    'use strict';
 
-var pivotal = require("pivotal"),
-    pivotalToken = process.env.PIVOTAL_TRACKER_TOKEN,
-    pivotalProject = process.env.PIVOTAL_TRACKER_PROJECT_ID;
+    var pivotal = require('pivotal'),
+        pivotalToken = process.env.PIVOTAL_TRACKER_TOKEN,
+        pivotalProject = process.env.PIVOTAL_TRACKER_PROJECT_ID;
 
-console.log('pivotalToken', pivotalToken);
-console.log('pivotalProject', pivotalProject);
+    pivotal.useToken(pivotalToken);
 
-pivotal.useToken(pivotalToken);
-
-exports.index = function(req, res) {
-  pivotal.getProjectActivities(pivotalProject, req.query, function (err, ret) {
-    if (!err) {
-      res.render(req.xhr ? '_items' : 'index', {
-        items: ret.activity ? [].concat(ret.activity) : []
-      });
-    } // else error
-  });
-
-};
+    exports.index = function(req, res) {
+        pivotal.getProjectActivities(pivotalProject, req.query, function (err, ret) {
+            if (!err) {
+                res.render(req.xhr ? '_items' : 'index', {
+                    items: ret.activity ? [].concat(ret.activity) : []
+                });
+            } // else error
+        });
+    };
+}());
